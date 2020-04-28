f7dc5a181d5bf82453aa03b6a81f6d7ee0f1f7cf
Zach Roark <zach.roark@yahoo.com>
2020-04-28 14:39:21 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,28 @@
+Data can be represented in many ways
+Take the following data and represent it in
+
+1. A string
+2. An object
+3. An array
+
+"lunch"
+"peanut butter and jelly"
+"whole wheat"
+"grape juice"
+"apple"
+
+var meal = "lunch", 
+var sandwhich = "peanut butter and jelly", 
+var bread = "whole wheat", 
+var drink = "grape juice", 
+var fruit = "apple"
+
+var data = {
+    "lunch", 
+    "peanut butter and jelly", 
+    "whole wheat", 
+    "grape juice", 
+    "apple"}
+
+var data = ["lunch", "peanut butter and jelly", 
+"whole wheat", "grape juice", "apple"]
